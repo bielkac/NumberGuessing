d45a7ac7abf79ce3667a50eaef923c9557d313e1
Bielka Cordero <bielka.corderohuerta.2022@student.lhschools.org>
2019-10-25 09:33:55 -0400

    scanner keyboard was intialized

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,18 @@
+package org.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner keyboard;
+
+        int chosedNumber;
+
+        for(int i = 0; i <30; i++) {
+        chosedNumber = (int) (Math.random()*2);
+
+        System.out.println(chosedNumber);
+    }
+}}
